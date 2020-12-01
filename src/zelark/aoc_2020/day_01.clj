9dfdb18e1188ea1157f2efd7c7fb57d614e98c45
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-01 23:36:53 +0300

    Add day 01

---
+++ src/zelark/aoc_2020/day_01.clj
@@ -0,0 +1,23 @@
+(ns zelark.aoc-2020.day-01
+  (:require [clojure.java.io :as io]))
+
+(def input
+  (->> (io/reader (io/resource "input_01.txt"))
+       line-seq
+       (map #(Long/parseLong %))))
+
+(defn part1 [numbers target]
+  (first
+   (for [[a & a-rest] (iterate next numbers) :while a
+         b a-rest :when (== (+ a b) target)]
+     (* a b))))
+
+(defn part2 [numbers target]
+  (first
+   (for [[a & a-rest] (iterate next numbers) :while a
+         [b & b-rest] (iterate next a-rest)  :while b
+         c            b-rest                 :when (== (+ a b c) target)]
+     (* a b c))))
+
+(part1 input 2020)
+(part2 input 2020)

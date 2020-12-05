a860fe30640e3e2c98750d667359f065fd5f6fc7
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-05 14:12:05 +0300

    Refact day 05

--- src/zelark/aoc_2020/day_05.clj
+++ src/zelark/aoc_2020/day_05.clj
@@ -5,21 +5,17 @@
 ;; --- Day 5: Binary Boarding ---
 ;; https://adventofcode.com/2020/day/5
 
-(def input (str/split-lines (slurp (io/resource "input_05.txt"))))
+(def input (slurp (io/resource "input_05.txt")))
 
-(defn- decode [code]
-  (let [b (->> code (map {\F 0 \B 1 \R 1 \L 0}) (apply str))]
-    (Long/parseLong b 2)))
-
-(defn decode-seat [code]
-  (let [[row col] (split-at 7 code)]
-    (+ (* (decode row) 8) (decode col))))
+(defn decode [code]
+  (-> (str/escape code {\F 0 \L 0 \B 1 \R 1})
+      (Long/parseLong 2)))
 
 ;; part 1
-(apply max (map decode-seat input)) ; 878
+(apply max (map decode (str/split-lines input))) ; 878
 
 ;; part 2
-(->> (map decode-seat input)
+(->> (map decode (str/split-lines input))
      (sort)
      (partition 2 1)
      (some (fn [[lo hi]] (when (= (+ lo 2) hi) (inc lo))))) ; 504

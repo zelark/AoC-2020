1db3c287bd716bc3f118d91d1a719ab609a3c1ff
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-06 08:36:02 +0300

    Add day 06

---
+++ src/zelark/aoc_2020/day_06.clj
@@ -0,0 +1,26 @@
+(ns zelark.aoc-2020.day-06
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+;; --- Day 6: Custom Customs ---
+;; https://adventofcode.com/2020/day/6
+
+(def input (slurp (io/resource "input_06.txt")))
+
+(defn parse-input [input]
+  (->> (str/split input #"\R\R")
+       (map (partial re-seq #"[a-z]+"))
+       (map (partial map set))))
+
+;; part 1
+(->> (parse-input input)
+     (map (partial apply set/union))
+     (map count)
+     (apply +)) ; 6735
+
+;; part 2
+(->> (parse-input input)
+     (map (partial apply set/intersection))
+     (map count)
+     (apply +)) ; 3221

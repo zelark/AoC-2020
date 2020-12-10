c3847254f9a705336a4a65edd8e1278cdd17eb90
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-10 10:17:04 +0300

    Refact day 10

--- src/zelark/aoc_2020/day_10.clj
+++ src/zelark/aoc_2020/day_10.clj
@@ -8,33 +8,28 @@
 (def input (slurp (io/resource "input_10.txt")))
 
 (defn parse-input [input]
-  (->> (str/split-lines input)
-       (map #(Long/parseLong %))))
+  (let [adapters (->> (str/split-lines input)
+                      (map #(Long/parseLong %))
+                      (apply sorted-set))]
+    (conj adapters 0 (+ (last adapters) 3))))
 
-(def parsed-input (parse-input input))
-(def charging-outlet 0)
-(def built-in (+ (apply max parsed-input) 3))
-(def adapters (sort (conj parsed-input charging-outlet built-in)))
+(def adapters (parse-input input))
+
+(defn find-next [adapter adapters]
+  (seq (filter adapters (map + [1 2 3] (repeat 3 adapter)))))
 
 ;; part 1
-(->> (for [adapter adapters
-           :let [jolts (set (map + [1 2 3] (repeat 3 adapter)))
-                 found (some jolts adapters)]
-           :when found]
-       (- found adapter))
+(->> adapters
+     (keep #(when-first [found (find-next % adapters)] (- found %)))
      (frequencies)
      (vals)
-     (apply +)) ; 2400
+     (apply *)) ; 2400
 
 ;; part 2
-(defn find-adapters [adapter adapters]
-  (let [jolts (set (map + [1 2 3] (repeat 3 adapter)))]
-    (filter jolts adapters)))
-
 (def count-arranges
   (memoize
    (fn [start]
-     (if-let [found (seq (find-adapters start adapters))]
+     (if-let [found (find-next start adapters)]
        (apply + (dec (count found)) (map count-arranges found))
        0))))
 

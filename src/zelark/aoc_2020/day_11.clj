e6b583ef9dfbccb0d9421025ead9d36c24816023
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-11 22:00:32 +0300

    Refact day 11

--- src/zelark/aoc_2020/day_11.clj
+++ src/zelark/aoc_2020/day_11.clj
@@ -7,52 +7,50 @@
 
 (def input (slurp (io/resource "input_11.txt")))
 
-(defn parse-input [input]
+(defn parse-input [input target]
   (->> (str/split-lines input)
-       (mapcat (fn [y row] (keep-indexed (fn [x ch] (when (= ch \L) [x y])) row))
+       (mapcat (fn [y row] (keep-indexed (fn [x ch] (when (= ch target) [x y])) row))
                (range))))
 
-(def seats (set (parse-input input)))
+(def seats (set (parse-input input \L)))
+(def floor (set (parse-input input \.))) ; needed for part 2
 
 (defn build-neighbours [find-fn seats]
   (reduce #(assoc %1 %2 (find-fn seats %2)) {} seats))
 
-(defn step [seats neighbours limit occupied?]
+(defn step [seats neighbours tolerance occupied?]
   (set (keep (fn [seat]
                (let [n (->> (neighbours seat) (filter occupied?) count)]
                  (when (or (and (not (occupied? seat)) (zero? n))
-                           (and (occupied? seat) (< n limit)))
+                           (and (occupied? seat) (< n tolerance)))
                    seat)))
              seats)))
 
 (defn solve [step]
-  (loop [occupied #{}]
-    (let [occupied' (step occupied)]
-      (if (= occupied' occupied)
-        (count occupied)
-        (recur occupied')))))
+  (->> (iterate step #{})
+       (partition 2 1)
+       (drop-while #(apply not= %))
+       ffirst count))
 
 ;; part 1
-(defn find-neighbours [seats [x y]]
+(defn find-near-neighbours [seats [x y]]
   (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)
         :let [seat [(+ x dx) (+ y dy)]]
         :when (seats seat)]
     seat))
 
-(solve (partial step seats (build-neighbours find-neighbours seats) 4)) ; 2359
+(let [neighbours (build-neighbours find-near-neighbours seats)]
+  (solve (partial step seats neighbours 4))) ; 2359
 
 ;; part 2
-(defn find-neighbours-2 [w h seats [x y]]
+(defn find-first-neighbours [floor seats [x y]]
   (->> (for [dx [-1 0 1] dy [-1 0 1] :when (not= 0 dx dy)]
-         (loop [x' (+ x dx) y' (+ y dy)]
-           (when (and (<= 0 x' w) (<= 0 y' h))
-             (if (contains? seats [x' y'])
-               [x' y']
-               (recur (+ x' dx) (+ y' dy))))))
+         (->> (iterate (fn [[x y]] [(+ x dx) (+ y dy)]) [x y])
+              rest
+              (drop-while floor)
+              first seats))
        (remove nil?)))
 
-(let [[w _] (apply max-key first seats)
-      [_ h] (apply max-key second seats)
-      find-fn (partial find-neighbours-2 w h)
+(let [find-fn (partial find-first-neighbours floor)
       neighbours (build-neighbours find-fn seats)]
   (solve (partial step seats neighbours 5))) ; 2131

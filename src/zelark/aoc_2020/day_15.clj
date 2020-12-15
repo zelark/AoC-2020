c9065eca755f74224e4d20c701e3cb515954267b
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-15 15:46:29 +0300

    Refact day 15: fast version

--- src/zelark/aoc_2020/day_15.clj
+++ src/zelark/aoc_2020/day_15.clj
@@ -1,29 +1,47 @@
 (ns zelark.aoc-2020.day-15
-  (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+  (:require [clojure.java.io :as io]))
 
 ;; --- Day 15: Rambunctious Recitation ---
 ;; https://adventofcode.com/2020/day/15
 
 (def input (slurp (io/resource "input_15.txt")))
 
-(defn parse-input [input]
-  (let [numbers (->> (str/split input #"[,\n]")
-                     (map #(Long/parseLong %)))]
-    (zipmap numbers (range 1 (inc (count numbers))))))
+(set! *unchecked-math* :warn-on-boxed)
+(set! *warn-on-reflection* true)
 
-(defn solve [input limit]
-  (loop [spoken (parse-input input)
-         n      (inc (count spoken))
-         number 0]
-    (if (== n limit)
+(defn parse-input [input]
+  (->> (re-seq #"\d+" input)
+       (mapv #(Long/parseLong %))))
+
+(defn solve [numbers ^long limit]
+  (loop [spoken (zipmap numbers (rest (range)))
+         turn   (count numbers)
+         number (peek numbers)]
+    (if (== turn limit)
       number
-      (if-let [last-time (spoken number)]
-        (recur (assoc spoken number n) (inc n) (- n last-time))
-        (recur (assoc spoken number n) (inc n) 0)))))
+      (if-let [last-spoken (spoken number)]
+        (recur (assoc spoken number turn) (inc turn) (- turn ^long last-spoken))
+        (recur (assoc spoken number turn) (inc turn) 0)))))
 
 ;; part 1
-(solve input 2020) ; 232
+(solve (parse-input input) 2020) ; 232
 
 ;; part 2
-(solve input 30000000) ; 18929178
+(solve (parse-input input) 30000000) ; 18929178
+
+;; bonus: fast version
+(defn solve-fast [numbers ^long limit]
+  (let [spoken (int-array limit)
+        _      (doseq [[^int i num] (map-indexed vector numbers)]
+                 (aset spoken num (inc i)))]
+    (loop [turn   (int (count numbers))
+           number (peek numbers)]
+      (if (== turn limit)
+        number
+        (let [last-spoken (aget spoken number)
+              _           (aset spoken number turn)]
+          (recur (unchecked-inc-int turn)
+                 (cond->> last-spoken
+                   (not= last-spoken 0) (unchecked-subtract-int turn))))))))
+
+(solve-fast (parse-input input) 30000000) ; ~800 ms

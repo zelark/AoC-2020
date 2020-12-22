dac4e6641f4f251ff1851059d59c5a342a1aa486
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-22 14:28:49 +0300

    Refact day 22

--- src/zelark/aoc_2020/day_22.clj
+++ src/zelark/aoc_2020/day_22.clj
@@ -7,61 +7,57 @@
 
 (def input (slurp (io/resource "input_22.txt")))
 
-(def queue clojure.lang.PersistentQueue/EMPTY)
+(defn queue [coll] (into clojure.lang.PersistentQueue/EMPTY coll))
 
 (defn parse-deck [s]
   (->> (str/split-lines s)
        (rest)
        (map #(Long/parseLong %))
-       (into queue)))
+       (queue)))
 
 (defn parse-input [input]
   (let [[deck1 deck2] (str/split input #"\R\R")]
     [(parse-deck deck1) (parse-deck deck2)]))
 
-(defn solve [play [deck1 deck2]]
-  (->> (play deck1 deck2)
-       :deck
-       (reverse)
+(defn score [{deck :deck}]
+  (->> (reverse deck)
        (map * (rest (range)))
        (apply +)))
 
 ;; part 1
-(defn play-game [deck1 deck2]
+(defn combat [deck1 deck2]
   (cond
-    (empty? deck1) {:deck deck2}
-    (empty? deck2) {:deck deck1}
+    (empty? deck1) {:deck deck2 :winner :crab}
+    (empty? deck2) {:deck deck1 :winer  :me}
     :else
-    (let [card1 (peek deck1)
-          card2 (peek deck2)]
+    (let [[card1 deck1] ((juxt peek pop) deck1)
+          [card2 deck2] ((juxt peek pop) deck2)]
       (if (< card1 card2)
-        (recur (pop deck1) (conj (pop deck2) card2 card1))
-        (recur (conj (pop deck1) card1 card2) (pop deck2))))))
+        (recur deck1 (conj deck2 card2 card1))
+        (recur (conj deck1 card1 card2) deck2)))))
 
-(solve play-game (parse-input input)) ; 30197
+(->> (parse-input input) (apply combat) score) ; 30197
 
 ;; part 2
-(defn play-recursive-game [deck1 deck2]
+(defn recursive-combat [deck1 deck2]
   (loop [deck1 deck1 deck2 deck2 history #{}]
     (cond
-      (history [deck1 deck2]) {:deck deck1 :winner :player1}
-      (empty? deck1) {:deck deck2 :winner :player2}
-      (empty? deck2) {:deck deck1 :winner :player1}
+      (history [deck1 deck2]) {:deck deck1 :winner :me}
+      (empty? deck1) {:deck deck2 :winner :crab}
+      (empty? deck2) {:deck deck1 :winner :me}
       :else
-      (let [card1    (peek deck1)
-            card2    (peek deck2)
-            deck1'   (pop deck1)
-            deck2'   (pop deck2)
-            history' (conj history [deck1 deck2])]
-        (if (and (<= card1 (count deck1'))
-                 (<= card2 (count deck2')))
-          (if (= (:winner (play-recursive-game (into queue (take card1 deck1'))
-                                               (into queue (take card2 deck2'))))
-                 :player1)
-            (recur (conj deck1' card1 card2) deck2' history')
-            (recur deck1' (conj deck2' card2 card1) history'))
+      (let [history (conj history [deck1 deck2])
+            [card1 deck1] ((juxt peek pop) deck1)
+            [card2 deck2] ((juxt peek pop) deck2)]
+        (if (and (<= card1 (count deck1))
+                 (<= card2 (count deck2)))
+          (if (= (:winner (recursive-combat (queue (take card1 deck1))
+                                            (queue (take card2 deck2))))
+                 :me)
+            (recur (conj deck1 card1 card2) deck2 history)
+            (recur deck1 (conj deck2 card2 card1) history))
           (if (< card1 card2)
-            (recur deck1' (conj deck2' card2 card1) history')
-            (recur (conj deck1' card1 card2) deck2' history')))))))
+            (recur deck1 (conj deck2 card2 card1) history)
+            (recur (conj deck1 card1 card2) deck2 history)))))))
 
-(solve play-recursive-game (parse-input input)) ; 34031
+(->> (parse-input input) (apply recursive-combat) score) ; 34031

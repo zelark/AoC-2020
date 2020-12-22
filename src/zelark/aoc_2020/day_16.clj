a654f03064c51f9ebf5ec40cddbce33235d89112
Aleksandr Zhuravlev <kvark.lark@gmail.com>
2020-12-22 17:04:17 +0300

    Refact day 16

--- src/zelark/aoc_2020/day_16.clj
+++ src/zelark/aoc_2020/day_16.clj
@@ -22,25 +22,17 @@
        my-ticket (parse-longs my-ticket)]
    {:rules rules :tickets tikets :my-ticket my-ticket}))
 
-(defn valid? [[_ [[a b] [c d]]] number]
+(defn valid-value? [[_ [[a b] [c d]]] number]
   (or (<= a number b)
       (<= c number d)))
-
-(defn find-invalid [rules ticket]
-  (keep #(when-not (some (fn [rule] (valid? rule %)) rules) %) ticket))
 
 ;; part 1
 (let [{:keys [rules tickets]} (parse-input input)]
   (->> (flatten tickets)
-       (remove (fn [n] (some #(valid? % n) rules)))
+       (remove (fn [n] (some #(valid-value? % n) rules)))
        (apply +))) ; 20013
 
 ;; part 2
-(defn find-fields [rules field]
-  (->> (filter (fn [rule] (every? #(valid? rule %) field)) rules)
-       (map first)
-       (set)))
-
 (defn narrow [coll]
   (loop [[[k v] & xs] (sort-by (comp count second) coll)
          result {}]
@@ -48,13 +40,23 @@
       result
       (recur (->> (map (fn [[kn vn]] [kn (disj vn (first v))]) xs)
                   (sort-by (comp count second)))
-             (assoc result (first v) k)))))
+             (assoc result k (first v))))))
 
-(let [{:keys [rules my-ticket tickets]} (parse-input input)]
-  (->> (remove #(seq (find-invalid rules %)) tickets)
-       (apply map vector)
-       (map-indexed #(-> [%1 (find-fields rules %2)]))
+(defn valid-ticket? [rules ticket]
+  (every? #(some (fn [rule] (valid-value? rule %)) rules) ticket))
+
+(defn valid-cols [rule cols]
+  (set (keep-indexed (fn [n values]
+                       (when (every? #(valid-value? rule %) values) n))
+                     cols)))
+
+(let [{:keys [rules tickets my-ticket]} (parse-input input)
+      columns (->> (filter #(valid-ticket? rules %) tickets)
+                   (apply map vector))]
+  (->> rules
+       (reduce (fn [m [field :as rule]]
+                 (assoc m field (valid-cols rule columns))) {})
        (narrow)
        (filter (fn [[field]] (str/starts-with? field "departure")))
-       (map (fn [[_ idx]] (my-ticket idx)))
+       (map (fn [[_ n]] (my-ticket n)))
        (apply *))) ; 5977293343129
